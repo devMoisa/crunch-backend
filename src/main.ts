2de7b27862149f5dbb837d2e2983d986889fb738
Moises Vilas Boas <moisesboas@gmail.com>
2024-06-03 03:47:20 -0300

    fix: swagger route

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ExpressAdapter } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
 import { NotFoundInterceptor } from './common/errors/interceptors/notFound.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const server = express();
@@ -27,6 +28,16 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Crunch Test')
+    .setDescription('API DevMoises to work with Hydrogen client-side')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   console.log(`HTTP Server running on port: ${port} 🚀`);
 }
